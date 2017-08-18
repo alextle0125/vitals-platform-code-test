cabc35c969ded545822a17929ad60d9dee3e004c
Alexander Le <alextle0125>
2017-08-18 18:23:42 -0400

    modify 'Blue Star' tests to activate tests

--- update_quality_spec.rb
+++ update_quality_spec.rb
@@ -177,9 +177,11 @@ describe '#update_quality' do
       end
 
       context 'given a Blue Star award' do
-        before { pending }
         let(:name) { 'Blue Star' }
-        before { award.expires_in.should == initial_expires_in-1 }
+        
+        before do
+          award.expires_in.should == initial_expires_in-1
+        end
 
         context 'before the expiration date' do
           let(:initial_expires_in) { 5 }
